ae149b78cdfbb32a94e6698edfb1c738c5d223e8
kory0115 <kory01167@gmail.com>
2024-05-07 11:25:53 +0900

    fix: size

--- app/src/main/java/com/example/searchinfo/repository/RetrofitRepository.kt
+++ app/src/main/java/com/example/searchinfo/repository/RetrofitRepository.kt
@@ -6,6 +6,6 @@ import retrofit2.Response
 
 class RetrofitRepository {
     suspend fun productApi(query: String, sort: String): Response<ImageResponse> {
-        return RetrofitFix.retrofitResult.getProducts(query = query, sort = sort, page = 1, size = 5)
+        return RetrofitFix.retrofitResult.getProducts(query = query, sort = sort, page = 1, size = 80)
     }
 }
