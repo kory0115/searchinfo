3052194a28cab00034a7a813e0655b6fef5092ae
kory0115 <kory01167@gmail.com>
2024-05-07 15:49:06 +0900

    add: okhttp3

--- app/src/main/java/com/example/searchinfo/data/RetrofitFix.kt
+++ app/src/main/java/com/example/searchinfo/data/RetrofitFix.kt
@@ -1,5 +1,7 @@
 package com.example.searchinfo.data
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,6 +10,16 @@ object RetrofitFix {
         Retrofit.Builder()
             .baseUrl(Url.KAKAO_API_KEY)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okhttpClient())
+            .build()
+
+    private fun okhttpClient() : OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    HttpLoggingInterceptor.Level.BODY
+                }
+            )
             .build()
 
     val retrofitResult : ProductApiService = retrofit?.create(ProductApiService::class.java)!!
